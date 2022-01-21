68fc1f99fdb410f7c33e5a3ed09a5fccd8d60c80
Francis Duval <francisduval37@gmail.com>
2022-01-21 16:31:51 -0500

    Define function that return the string vector of classic covariates

---
+++ R/make_classic_vars_vec.R
@@ -0,0 +1,14 @@
+make_classic_vars_vec <- function() {
+  c(
+    "annual_distance",
+    "commute_distance",
+    "conv_count_3_yrs_minor",
+    "gender",
+    "marital_status",
+    "pmt_plan",
+    "veh_age",
+    "veh_use",
+    "years_claim_free",
+    "years_licensed"
+  )
+}
