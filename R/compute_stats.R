03383a4e71b7f78d6b6eedfbb3a4a547e9062b88
Francis Duval <francisduval37@gmail.com>
2022-01-21 12:41:44 -0500

    Define function that computes different stat summaries in a data frame

---
+++ R/compute_stats.R
@@ -0,0 +1,21 @@
+compute_stats <- function(data, group, vars) {
+  data %>% 
+    group_by({{group}}) %>% 
+    summarise_at(
+      .vars = all_of(vars),
+      .funs = 
+        list(
+          mean = mean, 
+          var = var, 
+          skewness = skewness, 
+          kurtosis = kurtosis,
+          q01 = ~ quantile(., probs = 0.01),
+          q05 = ~ quantile(., probs = 0.05),
+          q25 = ~ quantile(., probs = 0.25),
+          q50 = ~ quantile(., probs = 0.5),
+          q75 = ~ quantile(., probs = 0.75),
+          q95 = ~ quantile(., probs = 0.95),
+          q99 = ~ quantile(., probs = 0.99)
+        )
+    )
+}
