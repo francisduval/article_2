805f034034455fa5267d785e78d4f8984512bd01
Francis Duval <francisduval37@gmail.com>
2022-02-03 15:20:30 -0500

    Delete useless functions

--- R/define_recipe.R
+++
@@ -1,9 +0,0 @@
-define_recipe <- function(data, covariates) {
-  recipe(claim_ind_cov_1_2_3_4_5_6 ~ ., data = data) %>%
-    update_role(-all_of(covariates), -claim_ind_cov_1_2_3_4_5_6, new_role = "id") %>%
-    step_other(all_nominal_predictors(), threshold = 0.05) %>%
-    step_lencode_glm(all_nominal_predictors(), outcome = "claim_ind_cov_1_2_3_4_5_6") %>%
-    step_impute_bag(all_predictors()) %>%
-    step_normalize(all_predictors()) %>%
-    step_YeoJohnson(all_predictors())
-}
