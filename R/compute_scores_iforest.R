a7e9357c8cc5b46cef8d9ccc838d58ae02b55518
Francis Duval <francisduval37@gmail.com>
2022-02-16 15:34:01 -0500

    Change sample size in isolation forest function

--- R/compute_scores_iforest.R
+++ R/compute_scores_iforest.R
@@ -1,5 +1,5 @@
 compute_scores_iforest <- function(data) {
-  iso = isolationForest$new()
+  iso = isolationForest$new(sample_size = min(256, nrow(data)))
   iso$fit(data)
   scores <- iso$predict(data)
   return(scores$anomaly_score)
