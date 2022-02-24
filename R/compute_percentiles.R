9310545846ffd83a99a967e3c03d75e26870a2a4
Francis Duval <francisduval37@gmail.com>
2022-02-24 15:25:56 -0500

    Compute also min and max

--- R/compute_percentiles.R
+++ R/compute_percentiles.R
@@ -1,5 +1,5 @@
 compute_percentiles <- function(data, vars) {
-  p <- seq(0.01, 0.99, by = 0.01)
+  p <- seq(0, 1, by = 0.01)
   p_names <- map_chr(p, ~ paste0("q_", .x * 100))
   p_funs <- map(p, ~ partial(quantile, probs = ., na.rm = T)) %>% set_names(nm = p_names)
   
