fd4080de8a34b6923e66406a84d2738dcae58e8c
Francis Duval <francisduval37@gmail.com>
2022-02-28 11:50:09 -0500

    Fraction of trips instead of absolute number

--- R/compute_local_lofs.R
+++ R/compute_local_lofs.R
@@ -1,7 +1,7 @@
-compute_local_lofs <- function(data, k) {
+compute_local_lofs <- function(data, k_frac) {
   data %>% 
     group_split(vin) %>% 
     map(bake_data_lof) %>% 
-    map(lof, minPts = k) %>% 
+    map(~ lof(., minPts = round(k_frac * nrow(.)))) %>% 
     unlist()
 }
