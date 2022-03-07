4d549656c2d7358f6abffd4611be92b750ebe01e
Francis Duval <francisduval37@gmail.com>
2022-03-07 14:23:06 -0500

    Target to compute local mahalanobis scores

--- _targets.R
+++ _targets.R
@@ -275,6 +275,11 @@ list(
   
   # ----------
   
+  tar_target(
+    local_maha,
+    compute_local_maha(aug_trip_sample_train)
+  ),
+  
   tar_target(local_lof_grid, seq(0.05, 0.6, by = 0.05)),
   tar_target(
     local_lof_train,
