531a8c8dc2cafdd06b7bb21b710918debd187480
Francis Duval <francisduval37@gmail.com>
2022-03-08 15:09:51 -0500

    Function to obtain performance metrics on new dataset

---
+++ R/test_en.R
@@ -0,0 +1,16 @@
+test_en <- function(fit, new_data) {
+  preds <- predict(fit, new_data = new_data, type = "prob")
+  preds_hard <- predict(fit, new_data = new_data)
+  
+  res <- 
+    tibble(
+      roc_auc = roc_auc_vec(x$claim_ind_cov_1_2_3_4_5_6, preds$.pred_0),
+      accuracy = accuracy_vec(x$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class),
+      mn_log_loss = mn_log_loss_vec(x$claim_ind_cov_1_2_3_4_5_6, preds$.pred_0),
+      f_meas = f_meas_vec(x$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class),
+      sensitivity = sensitivity_vec(x$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class),
+      specificity = specificity_vec(x$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class)
+  )
+  
+  return(res)
+}
