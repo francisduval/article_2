8f86a630a63612ff109c1726fd360c03b53513f6
Francis Duval <francisduval37@gmail.com>
2022-03-10 14:03:07 -0500

    Function to test a fitted workflow on new data

--- R/test_en.R
+++ R/test_en.R
@@ -4,12 +4,12 @@ test_en <- function(fit, new_data) {
   
   res <- 
     tibble(
-      roc_auc = roc_auc_vec(x$claim_ind_cov_1_2_3_4_5_6, preds$.pred_0),
-      accuracy = accuracy_vec(x$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class),
-      mn_log_loss = mn_log_loss_vec(x$claim_ind_cov_1_2_3_4_5_6, preds$.pred_0),
-      f_meas = f_meas_vec(x$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class),
-      sensitivity = sensitivity_vec(x$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class),
-      specificity = specificity_vec(x$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class)
+      roc_auc = roc_auc_vec(new_data$claim_ind_cov_1_2_3_4_5_6, preds$.pred_0),
+      accuracy = accuracy_vec(new_data$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class),
+      mn_log_loss = mn_log_loss_vec(new_data$claim_ind_cov_1_2_3_4_5_6, preds$.pred_0),
+      f_meas = f_meas_vec(new_data$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class),
+      sensitivity = sensitivity_vec(new_data$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class),
+      specificity = specificity_vec(new_data$claim_ind_cov_1_2_3_4_5_6, preds_hard$.pred_class)
   )
   
   return(res)
